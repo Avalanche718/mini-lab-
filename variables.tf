139494e46b144c4143f8d546119be35a830c05b0
Avalanche718 <uwolaayomide17@gmail.com>
2026-02-15 00:58:45 +0100

    Add variables for location and prefix in Terraform

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type    = string
+  default = "westeurope"   # or "northeurope", etc.
+}
+
+variable "prefix" {
+  type    = string
+  default = "jrlab"        # short prefix to avoid name conflicts
+}
