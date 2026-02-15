6a8d0ae5f68a939fc8e1acc4ae451ff87b31f7c9
Avalanche718 <uwolaayomide17@gmail.com>
2026-02-15 01:10:32 +0100

    Enhance outputs with descriptions and add resource group name
    Added descriptions for existing outputs and a new output for resource group name.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,13 @@
 output "logic_app_name" {
-  value = azurerm_logic_app_standard.logic.name
+  description = "Name of the Logic App Standard"
+  value       = azurerm_logic_app_standard.logic.name
 }
 
 output "integration_account_name" {
-  value = azurerm_integration_account.ia.name
+  description = "Name of the Integration Account"
+  value       = azurerm_integration_account.ia.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
